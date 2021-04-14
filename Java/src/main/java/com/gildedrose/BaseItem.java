f0c6f3da8a1a1b3c85152a75239fa7dfbf348fb5
Sjoerd Adema <sjoerd@kristalsoftware.nl>
2021-04-14 15:53:17 +0200

    [develop] refactored GildedRose to BaseItem and GenericItem

---
+++ Java/src/main/java/com/gildedrose/BaseItem.java
@@ -0,0 +1,7 @@
+package com.gildedrose;
+
+public interface BaseItem {
+
+    void updateQuality();
+
+}
--- Java/src/test/java/com/gildedrose/TexttestFixture.java
+++ Java/src/test/java/com/gildedrose/TexttestFixture.java
@@ -4,19 +4,19 @@ public class TexttestFixture {
     public static void main(String[] args) {
         System.out.println("OMGHAI!");
 
-        Item[] items = new Item[] {
-                new Item("+5 Dexterity Vest", 10, 20), //
-                new Item("Aged Brie", 2, 0), //
-                new Item("Elixir of the Mongoose", 5, 7), //
-                new Item("Sulfuras, Hand of Ragnaros", 0, 80), //
-                new Item("Sulfuras, Hand of Ragnaros", -1, 80),
-                new Item("Backstage passes to a TAFKAL80ETC concert", 15, 20),
-                new Item("Backstage passes to a TAFKAL80ETC concert", 10, 49),
-                new Item("Backstage passes to a TAFKAL80ETC concert", 5, 49),
+        BaseItem[] items = new BaseItem[]{
+                new GenericItem(new Item("+5 Dexterity Vest", 10, 20)), //
+                new GenericItem(new Item("Aged Brie", 2, 0)), //
+                new GenericItem(new Item("Elixir of the Mongoose", 5, 7)), //
+                new GenericItem(new Item("Sulfuras, Hand of Ragnaros", 0, 80)), //
+                new GenericItem(new Item("Sulfuras, Hand of Ragnaros", -1, 80)),
+                new GenericItem(new Item("Backstage passes to a TAFKAL80ETC concert", 15, 20)),
+                new GenericItem(new Item("Backstage passes to a TAFKAL80ETC concert", 10, 49)),
+                new GenericItem(new Item("Backstage passes to a TAFKAL80ETC concert", 5, 49)),
                 // this conjured item does not work properly yet
-                new Item("Conjured Mana Cake", 3, 6) };
+                new GenericItem(new Item("Conjured Mana Cake", 3, 6))};
 
-        GildedRose app = new GildedRose(items);
+//        GildedRose app = new GildedRose(items);
 
         int days = 2;
         if (args.length > 0) {
@@ -26,11 +26,11 @@ public class TexttestFixture {
         for (int i = 0; i < days; i++) {
             System.out.println("-------- day " + i + " --------");
             System.out.println("name, sellIn, quality");
-            for (Item item : items) {
+            for (BaseItem item : items) {
                 System.out.println(item);
+                item.updateQuality();
             }
             System.out.println();
-            app.updateQuality();
         }
     }
 
